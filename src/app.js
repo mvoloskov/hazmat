201e359e8994e571ec606fbee1b9359034c6ec1a
mvoloskov <hello@miloslav.website>
2021-05-18 23:04:38 +0300

    better post detection

--- src/app.js
+++ src/app.js
@@ -1,6 +1,20 @@
-const wrapperSelector = `.ListingLayout-outerContainer > *:nth-child(2) > *:nth-child(3) > *:first-child`
-const parentSelector = `.Post`
-const tagSelector = `[data-click-id="subreddit"]`
+const tagSelector = '[data-click-id="subreddit"]'
+const postSelector = '.Post'
+
+const isPost = node => node.classList.contains('Post')
+
+const getPostByLink = linkNode => {
+    const parent = linkNode.parentNode
+    if (!parent) return linkNode
+    if (isPost(parent)) return parent
+    return getPostByLink(parent)
+}
+
+const getPostsContainer = (onePostNode = document.querySelector(postSelector)) => {
+    const parent = onePostNode.parentNode
+    if (parent.childElementCount > 1) return parent
+    return getPostsContainer(parent)
+}
 
 const debounce = (fn, wait = 100) => {
     // this is double debounce
@@ -50,19 +64,36 @@ const start = () => {
     const handleChanges = () => debounce(chrome.storage.sync.get(null, store => {
         const bannedTags = (store.bannedWords || []).map(tag => tag.toLowerCase())
         if (bannedTags.length === 0) return
-        document.querySelectorAll(parentSelector).forEach(parent => {
-            const tag = parent.querySelector(tagSelector)?.getAttribute('href')
+
+        const links = document.querySelectorAll(tagSelector)
+
+        links.forEach(link => {
+            const linkText = link.getAttribute('href')
                 .replace('r/', '')
                 .replaceAll('/', '')
                 .toLowerCase()
-            if (!bannedTags.includes(tag) && !bannedTags.includes('r/' + tag)) return
-            parent.remove()
+
+            const shouldBlock =
+                bannedTags.includes(linkText) ||
+                bannedTags.includes('r/' + linkText)
+
+            if (!shouldBlock) return
+
+            const post = getPostByLink(link)
+            if (!post.remove) return
+            post.remove()
         })
     }))
 
+    const postsContainer = getPostsContainer()
+    if (!postsContainer) {
+        console.error('Hazmat: It seems like Reddit UI is changed. Hazmat needs to be updated to support a new Reddit. Please open an issue at https://github.com/mvoloskov/hazmat to get Hazmat updated.')
+        return
+    }
+
     handleChanges()
     observer = new MutationObserver(handleChanges)
-    observer.observe(document.querySelector(wrapperSelector), {
+    observer.observe(postsContainer, {
         childList: true,
     })
 }
